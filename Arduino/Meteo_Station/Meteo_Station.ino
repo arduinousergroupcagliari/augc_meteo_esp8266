0535f993d194ec7de72aa7314d5109586b2846f1
PaoloP74 <p.paolucci@tiscali.it>
2021-01-05 14:12:23 +0100

    Firmware 1216
    Update WifiManager to 0.16.0

--- Arduino/Meteo_Station/Meteo_Station.ino
+++ Arduino/Meteo_Station/Meteo_Station.ino
@@ -25,7 +25,7 @@ hp_BH1750 myBH1750;
 Adafruit_BME280 myBME280;
 WiFiClient client;
 
-const int FW_VERSION = 1215;
+const int FW_VERSION = 1216;
 bool DEV_VERSION = true;
 const char* fwServerBase = "raw.githubusercontent.com";
 const char* fwDirBase = "/arduinousergroupcagliari/augc_meteo_esp8266/LittleFS/bin/";
